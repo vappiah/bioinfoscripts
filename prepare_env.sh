e2b93981cf040b374f91f3a132076f020899146e
vincentappiah <appiahv@rocketmail.com>
2022-09-29 11:11:00 +0200

    Add files via upload

---
+++ prepare_env.sh
@@ -0,0 +1,20 @@
+wget https://github.com/ParBLiSS/FastANI/releases/download/v1.33/fastANI-Linux64-v1.33.zip
+unzip fastANI-Linux64-v1.33.zip -d /home/manager/bacterial_genomics/scripts
+
+echo "export PATH=$PATH:/home/manager/bacterial_genomics/scripts" >> $HOME/.bashrc 
+source $HOME/.bashrc
+
+wget https://github.com/vappiah/bioinfoscripts/raw/main/workshopfiles.zip
+unzip workshopfiles.zip -d scripts
+cp scripts/environment.yml ./
+
+conda init bash
+conda config --set auto_activate_base false
+source /home/manager/.bashrc
+
+conda config --add channels conda-forge
+conda config --add channels bioconda
+
+conda env create -f environment.yaml
+conda create -n prokka -c bioconda -c conda-forge -c defaults prokka
+
