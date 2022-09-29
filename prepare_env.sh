810afd6b45645d41f50ce3de12a450c4519d8427
vincentappiah <appiahv@rocketmail.com>
2022-09-29 11:54:54 +0200

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,17 +1,15 @@
 
-mkdir scripts
-mkdir tmp
+mkdir /home/manager/bacterial_genomics/scripts
+
 wget https://github.com/ParBLiSS/FastANI/releases/download/v1.33/fastANI-Linux64-v1.33.zip
-unzip fastANI-Linux64-v1.33.zip 
-mv fastANI /home/manager/bacterial_genomics/scripts
+unzip fastANI-Linux64-v1.33.zip -d /home/manager/bacterial_genomics/scripts
 
 echo "export PATH=$PATH:/home/manager/bacterial_genomics/scripts" >> $HOME/.bashrc 
 source $HOME/.bashrc
 
 wget https://github.com/vappiah/bioinfoscripts/raw/main/workshopfiles.zip
-unzip workshopfiles.zip -d tmp
-mv tmp/finalscripts/* scripts
-cp scripts/environment.yml ./
+unzip workshopfiles.zip -d /home/manager/bacterial_genomics/scripts
+cp /home/manager/bacterial_genomics/scripts/environment.yml ./
 
 conda init bash
 conda config --set auto_activate_base false
